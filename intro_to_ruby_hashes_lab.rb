8231f33a952ea6320526cd7b49b77706389d4469
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-06 23:40:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,8 @@ def monopoly_with_second_tier
   monopoly = {  
     :railroads => {
 	    :pieces => {},
-	    :rent_in_dollars => {},
-	    :names => {}
+	    :names => {},
+	    :rent_in_dollars => {}
     }
   }
 end
